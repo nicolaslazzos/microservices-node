b70ec0d372fa639ad0343ad129d3782c82e98d86
nicolaslazzos <nicolaslazzos09@gmail.com>
2021-01-17 22:26:37 -0300

    handling current user request

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,17 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-  res.send("current user");
+router.get("/api/users/currentuser", (req, res, next) => {
+  if (!req.session?.jwt) return res.send({ user: null });
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ user: payload });
+  } catch (e) {
+    res.send({ user: null });
+  }
 });
 
 export { router as currentUserRouter };
