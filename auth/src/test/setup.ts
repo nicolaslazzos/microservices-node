75e0862a0e4d5db11c285aac27554f9a771137af
nicolaslazzos <nicolaslazzos09@gmail.com>
2021-01-18 21:10:46 -0300

    writing some test for the signout and currentuser routes

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,15 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request from "supertest";
 import { app } from "../app";
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>;
+    }
+  }
+}
 
 let mongo: any;
 
@@ -28,3 +37,14 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({ email: "nicolaslazzos@gmail.com", password: "password" })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie");
+
+  return cookie;
+};
