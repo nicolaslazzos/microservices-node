57dbaf07dbbf103f385a527a535c80e2ebc54d7e
nicolaslazzos <nicolaslazzos09@gmail.com>
2021-01-30 19:43:15 -0300

    initializing microservice to handle the orders

---
+++ orders/src/app.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import cookieSession from "cookie-session";
+
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from "@nlazzos/gittix-common";
+
+const app = express();
+
+// to trust the nginx proxy
+app.set("trust proxy", true);
+
+// middleware
+app.use(express.json({ extended: false } as any));
+
+// signed: false => not encrypted
+// secure: true => only works in a https connection
+app.use(
+  cookieSession({ signed: false, secure: process.env.NODE_ENV !== "test" })
+);
+
+app.use(currentUser);
+
+// routing
+
+// not handled routes
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
+// error handling
+app.use(errorHandler);
+
+export { app };
---
+++ orders/src/index.ts
@@ -0,0 +1,58 @@
+import { app } from "./app";
+import mongoose from "mongoose";
+import { natsWrapper } from "./nats-wrapper";
+
+const start = async () => {
+  if (!process.env.JWT_KEY)
+    throw new Error("Environment variable JWT_KEY not found");
+
+  if (!process.env.MONGO_URI)
+    throw new Error("Environment variable MONGO_URI not found");
+
+  if (!process.env.NATS_URL)
+    throw new Error("Environment variable NATS_URL not found");
+
+  if (!process.env.NATS_CLUSTER_ID)
+    throw new Error("Environment variable NATS_CLUSTER_ID not found");
+
+  if (!process.env.NATS_CLIENT_ID)
+    throw new Error("Environment variable NATS_CLIENT_ID not found");
+
+  try {
+    // the clientId (second parameter) must be unique for every copy of the service (see deployment .yml file)
+
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed");
+
+      // exits the entire program
+      process.exit();
+    });
+
+    // handling interrupts or terminates to close the connection properly so the next time we connect,
+    // we will get the unprocessed events, otherwise, nats will think that the service is not going to connect again
+    // to test that we can delete the nats-depl pod
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+
+    console.log("Connected to MongoDB");
+  } catch (e) {
+    console.log("[start]", e);
+  }
+  const PORT = process.env.PORT || 3000;
+
+  app.listen(3000, () => console.log(`Running server on port ${PORT}`));
+};
+
+start();
