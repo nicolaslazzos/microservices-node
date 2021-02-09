d316ec23528d167b826e5c00535ff1824d343edf
nicolaslazzos <nicolaslazzos09@gmail.com>
2021-02-08 23:50:52 -0300

    initializing payments service

---
+++ payments/src/nats-wrapper.ts
@@ -0,0 +1,32 @@
+import nats, { Stan } from "node-nats-streaming";
+
+class NatsWrapper {
+  private _client?: Stan;
+
+  get client() {
+    if (!this._client) {
+      throw new Error("Cannot access NATS client before connecting");
+    }
+
+    return this._client;
+  }
+
+  connect(clusterId: string, clientId: string, url: string): Promise<void> {
+    // the clusterId is the one we defined in the nats deployment
+    this._client = nats.connect(clusterId, clientId, { url });
+
+    return new Promise((resolve, reject) => {
+      this.client.on("connect", () => {
+        console.log("Connected to NATS");
+        resolve();
+      });
+
+      this.client.on("error", (err) => {
+        console.log("Error when connecting to NATS");
+        reject(err);
+      });
+    });
+  }
+}
+
+export const natsWrapper = new NatsWrapper();
